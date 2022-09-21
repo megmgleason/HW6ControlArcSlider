bfa61b553ae472cd10be50b594a111dca0e783a4
megmgleason <marymargaretgleason@gmail.com>
2022-09-21 16:14:42 -0400

    Initial Commit

---
+++ HW6ControlArcSlider/HW6ControlArcSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW6ControlArcSliderApp.swift
+//  HW6ControlArcSlider
+//
+//  Created by MEG G on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HW6ControlArcSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
